57a10b3874c8b39f2d1301e51c69fada662f7642
Ashley Hawkins <awhawkins@proton.me>
2025-12-06 20:04:12 +0000

    WIP UI

---
+++ Assets/RankSuitDisplayUI.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+using static Card;
+
+public class RankSuitDisplayUI : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] rankSprites;
+    [SerializeField]
+    private Sprite[] suitSprites;
+
+    [SerializeField]
+    private Image suitRenderer;
+    [SerializeField]
+    private Image rankRenderer;
+    void UpdateDisplay(Rank rank, Suit suit)
+    {
+        suitRenderer.sprite = suitSprites[(int)suit];
+        rankRenderer.sprite = rankSprites[(int)rank];
+    }
+
+    void Start()
+    {
+        // Test:
+        UpdateDisplay(Rank.N5, Suit.Diamond);
+    }
+}
