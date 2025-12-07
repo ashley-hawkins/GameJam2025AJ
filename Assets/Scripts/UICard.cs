339f4507cf2eab710e83ba31f659d4ff372b5e99
Ashley Hawkins <awhawkins@proton.me>
2025-12-07 03:39:38 +0000

    More UI functionality

--- Assets/Scripts/UICard.cs
+++ Assets/Scripts/UICard.cs
@@ -1,17 +1,47 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UICard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    private bool isHovered = false;
+    private bool isSelected = false;
+    private int raisedAmount = 0;
+    [SerializeField]
+    private Transform innerTransform;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // transform.localPosition.y
+        isHovered = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        isHovered = false;
     }
 
+    public void Select()
+    {
+        isSelected = true;
+    }
+
+    public void Deselect()
+    {
+        isSelected = false;
+    }
+
+    public void Toggle()
+    {
+        isSelected = !isSelected;
+    }
+
+    public void Update()
+    {
+        var targetAmount = (isSelected ? 12 : 0) + (isHovered ? 9 : 0);
+        if (raisedAmount < targetAmount)
+            raisedAmount++;
+        else if (raisedAmount > targetAmount)
+            raisedAmount--;
+        innerTransform.localPosition = new Vector3(innerTransform.localPosition.x, raisedAmount / 3, innerTransform.localPosition.z);
+    }
 }
